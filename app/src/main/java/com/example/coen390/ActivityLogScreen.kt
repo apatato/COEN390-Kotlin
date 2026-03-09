97dd5270acdd86579f19a64fc99bc13cb0600770
Nick Vergados <vergadosn@gmail.com>
2026-03-09 00:58:39 -0400

    update to most recent IDE + added back button to activity log screen

--- app/src/main/java/com/example/coen390/ActivityLogScreen.kt
+++ app/src/main/java/com/example/coen390/ActivityLogScreen.kt
@@ -1,5 +1,6 @@
 package com.example.coen390
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,6 +21,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,8 +44,25 @@ class ActivityLogScreen : ComponentActivity() {
 
 @Composable
 fun ActivityLog(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     Scaffold(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            IconButton(
+                onClick = {
+                    val back = Intent(context, WelcomeScreen::class.java)
+                    context.startActivity(back)
+                },
+                modifier = Modifier.padding(start = 8.dp, top = 4.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = stringResource(id = R.string.back_icon_description),
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.Black
+                )
+            }
+        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
