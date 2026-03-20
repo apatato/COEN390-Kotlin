6343ff7ac96e0b70cf0f33684fcbcfff54c7e281
Nick Vergados <vergadosn@gmail.com>
2026-03-20 19:58:15 -0400

    create db

---
+++ app/src/main/java/com/example/coen390/Times.kt
@@ -0,0 +1,3 @@
+package com.example.coen390
+
+data class Times(val id: Int, val time: Float)
---
+++ app/src/main/java/com/example/coen390/TimesDatabaseHelper.kt
@@ -0,0 +1,41 @@
+package com.example.coen390
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class TimesDatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTable = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_TIME REAL)"
+        db?.execSQL(createTable)
+    }
+
+    override fun onUpgrade(
+        p0: SQLiteDatabase?,
+        p1: Int,
+        p2: Int
+    ) {
+        val dropTable = "DROP TABLE IF EXISTS $TABLE_NAME"
+        p0?.execSQL(dropTable)
+        onCreate(p0)
+    }
+
+    fun insertTime(time: Times) {
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TIME, time.time)
+        }
+        db.insert(TABLE_NAME, null, values)
+        db.close()
+    }
+
+    companion object{
+        const val DATABASE_NAME = "times.db"
+        const val DATABASE_VERSION = 1
+        const val TABLE_NAME = "times"
+        const val COLUMN_ID = "id"
+        const val COLUMN_TIME = "time"
+
+    }
+}
